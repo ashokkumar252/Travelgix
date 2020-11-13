dd21b85c31e7a8ffad0399971250b9edd3da5cbc
ashokkumar252 <ashokramasubhu@gmail.com>
2020-11-13 19:51:13 +0530

    13nov2020

--- Batch file execution/Scenario1.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\\shiplogix\Project
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\Scenario1.xml
-pause
