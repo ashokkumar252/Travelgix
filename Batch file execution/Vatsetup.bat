b07513bb13fe298c8a6874a8312e4cfb4e460c3b
ashokkumar252 <ashokramasubhu@gmail.com>
2020-12-04 02:00:48 +0530

    04dec2020

--- Batch file execution/Vatsetup.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\\Travlogix\Project
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\Vatsetup.xml
-pause
--- Scenario1.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Wastaautomation\Project
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\Scenario4.xml
-pause
